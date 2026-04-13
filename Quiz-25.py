42b13f68eab616ea3bac004d04a308ae6d3c9ce7
Niko-chemical-programming <nmammadli2500@gmail.com>
2026-04-13 22:10:51 +0400

    Quiz 25 sual [1-2]

--- Quiz-25.py
+++ Quiz-25.py
@@ -7,3 +7,13 @@ rand2 = random.randint(0, 12)
 print(suits[rand1], values[rand2])
 """
 # Task 2
+def FindSumofTerms(N):
+    for i in N:
+        sum = 0
+        for x in range(i+1):
+            sum += (3+2*x)/2**x
+        print(f"N = {i} sum= {sum}")
+
+N = [3, 5, 10, 15, 50, 100, 1000]
+FindSumofTerms(N)
+
