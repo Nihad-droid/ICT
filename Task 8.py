40105ddf71914597a38ce27aef66df5184ba26d3
Niko-chemical-programming <nmammadli2500@gmail.com>
2026-04-19 01:13:10 +0400

    Task 8 sual [1-9]

--- Task 8.py
+++ Task 8.py
@@ -117,4 +117,172 @@ for i in range(len(Half2)):
         if Half2[j] > Half2[j+1]:
             Half2[j], Half2[j+1] = Half2[j+1], Half2[j]
 print("Sorted_2 =", Half1+Half2)
+
+# Sual 6
+import random
+massiv = [random.randint(-100, 100) for _ in range(6)]
+new_massiv = []
+print(massiv)
+for i in massiv:
+    if i > 0:
+        new_massiv.append(i)
+for i in massiv:
+    if i <= 0:
+        new_massiv.append(i)
+
+for i in range(len(new_massiv)):
+    for j in range(0, len(new_massiv)-1):
+        if new_massiv[j] > 0 and new_massiv[j+1] > 0:
+            if new_massiv[j] < new_massiv[j+1]:
+                new_massiv[j], new_massiv[j+1] = new_massiv[j+1], new_massiv[j]
+        elif new_massiv[j] <= 0 and new_massiv[j+1] <= 0:
+            if new_massiv[j] > new_massiv[j+1]:
+                new_massiv[j], new_massiv[j+1] = new_massiv[j+1], new_massiv[j]
+print(new_massiv)
+
+# Sual 7
+import random
+massiv1 = [random.randint(-10000, 10000) for _ in range(1000)]
+massiv2 = [random.randint(-10000, 10000) for _ in range(1000)]
+massiv3 = [random.randint(-10000, 10000) for _ in range(1000)]
+massiv4 = [random.randint(-10000, 10000) for _ in range(1000)]
+massiv5 = [random.randint(-10000, 10000) for _ in range(1000)]
+massivler1 = [massiv1, massiv2, massiv3, massiv4, massiv5]
+massivler2 = []
+
+for massiv in massivler1:
+    new_sub_list = []
+    for item in massiv:
+        new_sub_list.append(item)
+    massivler2.append(new_sub_list)
+swaps1 = 0
+for massiv in massivler1:
+    for i in range(len(massiv)):    # Bubble Sort alqoritmi
+        for j in range(0, len(massiv)-1):
+            if massiv[j] > massiv[j+1]:
+                massiv[j], massiv[j+1] = massiv[j+1], massiv[j]
+                swaps1 += 1
+average_swaps1 = swaps1/5
+swaps2 = 0
+for massiv in massivler2:
+    for i in range(len(massiv)):    # Selective Sort alqoritmi
+        min_index = i
+        for j in range(i+1, len(massiv)):
+            if massiv[j] < massiv[min_index]:
+                min_index = j
+        massiv[i], massiv[min_index] = massiv[min_index], massiv[i]
+        swaps2 += 1
+average_swaps2 = swaps2/5
+print('bubble sort:', average_swaps1, 'selective sort:', average_swaps2)
+
+# Sual 8
+import random
+X = int(input("X ededini daxil edin: "))
+massiv = [random.randint(1,10) for _ in range(10, 20)]
+print(massiv)
+for i in range(len(massiv)):
+    for x in range(0, len(massiv)-1):
+        if massiv[x] > massiv[x+1]:
+            massiv[x], massiv[x+1] = massiv[x+1], massiv[x]
+print(massiv)
+
+count = 0
+low = 0
+high = len(massiv)-1
+found = False
+while low <= high:
+    mid = (low + high) // 2
+    count += 1
+    if massiv[mid] == X:
+        found = True
+        break
+    elif massiv[mid] < X:
+        low = mid + 1
+    else:
+        high = mid - 1
+if found:
+    print(X, 'tapildi!', 'muqayiselerin sayi:', count)
+else:
+    print(X, 'tapilmadi!')
+
+# Sual 9
+import random
+X = int(input("X ededini daxil edin: "))
+massiv = [random.randint(1,10) for _ in range(10, 20)]
+print(massiv)
+for i in range(len(massiv)):
+    for x in range(0, len(massiv)-1):
+        if massiv[x] > massiv[x+1]:
+            massiv[x], massiv[x+1] = massiv[x+1], massiv[x]
+print(massiv)
+
+
+def first_occurence(massiv, X):
+    low = 0
+    high = len(massiv)-1
+    first_index = -1
+    while low <= high:
+        mid = (low + high) // 2
+        if massiv[mid] == X:
+            first_index = mid
+            high = mid - 1  # Sola dogru get
+        elif massiv[mid] < X:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return first_index
+
+def last_occurence(massiv, X):
+    low, high = 0, len(massiv) - 1
+    last_idx = -1
+    while low <= high:
+        mid = (low + high) // 2
+        if massiv[mid] == X:
+            last_idx = mid
+            low = mid + 1  # Saga dogru get
+        elif massiv[mid] < X:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return last_idx
+
+first = first_occurence(massiv, X)
+last = last_occurence(massiv, X)
+
+if first != -1:
+    count = last- first + 1
+    print(f"{X} {count} defe var.")
+else:
+    print("Tapilmadi!")
 """
+# Sual 10
+import random
+X = int(input("X ededini daxil edin: "))
+massiv = [random.randint(1,10) for _ in range(10, 20)]
+print(massiv)
+for i in range(len(massiv)):
+    for x in range(0, len(massiv)-1):
+        if massiv[x] > massiv[x+1]:
+            massiv[x], massiv[x+1] = massiv[x+1], massiv[x]
+print(massiv)
+
+
+def binary_search(massiv, X):
+    low = 0
+    high = len(massiv)-1
+    found = False
+    while low <= high:
+        mid = (low + high) // 2
+        if massiv[mid] == X:
+            found = True
+            break
+        elif massiv[mid] < X:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return found, massiv[mid]
+
+if not binary_search(massiv, X)[0]:
+    print(F"{X} tapilmadi. Ona en yaxin {binary_search(massiv, X)[1]}")
+else:
+    print(f"{X} tapildi.")
